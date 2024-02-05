a9aa17c30424d4fbcbbaa16337588d920e4f6ad0
Daniel_pc_casa <daniel_santos300@hotmail.com>
2024-02-05 19:46:57 -0300

    [feat] adicionando todas as rotas

--- handler.py
+++ handler.py
@@ -1,7 +1,27 @@
 import hook_vendas
-import json
 
-def hello(event, context):
+def vendas(event, context):
     body = hook_vendas.criar_dados_vendas()
     
     return body
+
+def clientes(event, context):
+    body = hook_vendas.criar_dados_clientes()
+    
+    return body
+
+def produtos(event, context):
+    body = hook_vendas.criar_dados_produtos()
+    
+    return body
+
+def metodos_pagamentos(event, context):
+    body = hook_vendas.criar_dados_metodos_pagamento()
+    
+    return body
+
+def locais(event, context):
+    body = hook_vendas.criar_dados_locais()
+    
+    return body
+
